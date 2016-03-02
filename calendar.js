b311b33185e6e657a3042ce41eb2889ef1ddbf13
Stanley Stuart <stanley@stan.li>
2016-03-02 15:01:53 -0600

    interview checkin

--- calendar.js
+++ calendar.js
@@ -1,3 +1,44 @@
-var Calendar = function() {};
+var Calendar = function() {
+};
+
+Calendar.prototype = {
+
+  parse: function(input) {
+    var overlapped = [];
+
+    var sortedInput = input.slice().sort((a, b) => {
+      return a[0] < b[0];
+    });
+
+    sortedInput.forEach(function(timeSet, index) {
+      var startTime = timeSet[0];
+      var endTime = timeSet[1];
+
+      var rest = sortedInput.slice(index + 1);
+
+      console.log('rest!', rest);
+
+      rest.forEach(function(restTimeSet) {
+        var bStartTime = restTimeSet[0];
+        var bEndTime = restTimeSet[1];
+
+        var alreadyInOverlapped = overlapped.find(timeSet => {
+          return startTime === timeSet[0] && endTime === timeSet[1];
+        });
+
+        if (startTime === bStartTime && endTime === bEndTime) {
+          return;
+        }
+
+        if (!alreadyInOverlapped && startTime >= bStartTime && endTime <= bEndTime) {
+          overlapped.push(timeSet);
+        }
+      });
+    });
+
+    return overlapped.map(o => `(${o[0]}, ${o[1]})`).join(' ');
+  },
+
+};
 
 module.exports = Calendar;
--- calendar_tests.js
+++ calendar_tests.js
@@ -6,5 +6,28 @@ describe('Testing Calendar Conflicts', function(done){
       var calendar = new Calendar();
       chai.expect(typeof calendar).to.be.equal('object');
     });
+
+    describe('parsing a list of calendar times', function() {
+      it('shows the times that overlaps', function() {
+
+        var calendar = new Calendar();
+        var input = [
+          [1, 3],
+          [12, 24],
+          [2, 4],
+          [13, 15],
+          [5, 10]
+        ];
+
+        var output = '(1, 3), (12, 14), (2, 4), (13, 15)';
+
+        var parsed = calendar.parse(input);
+
+        console.log('parsed', parsed);
+        console.log('output', output);
+
+        chai.expect(calendar.parse(input)).to.equal(output);
+      });
+    });
   });
 });
